ae1efc82c0b8b8f7eea81dd019b659ddf04a8c65
Dariusk99 <dariuszkosnicki@gmail.com>
2026-06-07 07:56:13 +0200

    dodano log dla 100 plikow

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,16 @@
 
 if [ "$1" = "--date" ]; then
     date
+
+elif [ "$1" = "--logs" ]; then
+
+    for i in {1..100}
+    do
+        nazwa="log${i}.txt"
+
+        echo "Nazwa pliku: $nazwa" > "$nazwa"
+        echo "Utworzony przez: $(basename "$0")" >> "$nazwa"
+        echo "Data: $(date)" >> "$nazwa"
+    done
+
 fi
